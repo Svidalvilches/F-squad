219f6638034c7cb29c6ebe0a2962b5948c999c4c
Svidalvilches <84938738+Svidalvilches@users.noreply.github.com>
2022-02-09 08:47:12 -0700

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializedField] private float attackCooldown;
+    [SerializedField] private float range;
     [SerializedField] private int damage;
+    [SerializedField] private BoxCollider2D boxCollider;
+    [SerializedField] private LayerMask playerLayer;
     private float cooldownTimer = Math.Infinity;
 
     private void Update()
@@ -23,6 +26,16 @@ public class Enemy : MonoBehaviour
 
     private bool PlayerInSight()
     {
-        return false;
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localSclae.x,
+        new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
+        0, Vector2.left, 0, playerLayer);
+        return hit.collider != null;
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(BoxCollider.bounds.center + transform.right * range * transform.localSclae.x, 
+            new Vector3(boxCillider.bounds.size.x *range, boxCillider.bounds.size.y, boxCillider.bounds.size.z));
     }
 }
