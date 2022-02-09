ea39c3fe83fc6e5edbad00dfaf598bab65faf8a2
Svidalvilches <84938738+Svidalvilches@users.noreply.github.com>
2022-02-09 09:00:50 -0700

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializedField] private float attackCooldown;
-    [SerializedField] private float range;
-    [SerializedField] private int damage;
-    [SerializedField] private BoxCollider2D boxCollider;
-    [SerializedField] private LayerMask playerLayer;
+    private float attackCooldown;
+    private float range;
+    private int damage;
+    private BoxCollider2D boxCollider;
+    private LayerMask playerLayer;
     private float cooldownTimer = Math.Infinity;
 
     private void Update()
@@ -16,7 +16,7 @@ public class Enemy : MonoBehaviour
         //Attack only when player in sight
         if (PlayerInSight())
         {
-            if (cooldownTimer >= attactCooldown)
+            if (cooldownTimer >= attackCooldown)
             {
                 //Atack
             }
@@ -26,16 +26,15 @@ public class Enemy : MonoBehaviour
 
     private bool PlayerInSight()
     {
-        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localSclae.x,
-        new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
-        0, Vector2.left, 0, playerLayer);
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x,
+        new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z), 0, Vector2.left, 0, playerLayer);
         return hit.collider != null;
     }
     
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(BoxCollider.bounds.center + transform.right * range * transform.localSclae.x, 
-            new Vector3(boxCillider.bounds.size.x *range, boxCillider.bounds.size.y, boxCillider.bounds.size.z));
+        Gizmos.DrawWireCube(BoxCollider.bounds.center + transform.right * range * transform.localScale.x, 
+            new Vector3(boxCollider.bounds.size.x *range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
     }
 }
