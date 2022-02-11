6a5da02c15412dc99044c790257fd3ac673b0591
Svidalvilches <84938738+Svidalvilches@users.noreply.github.com>
2022-02-11 09:31:37 -0700

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -1,40 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private float attackCooldown;
-    private float range;
-    private int damage;
-    private BoxCollider2D boxCollider;
-    private LayerMask playerLayer;
-    private float cooldownTimer = Math.Infinity;
+    public GameObject player;
+    public float speed;
+    public float range;
 
-    private void Update()
-    {
-        cooldownTimer += Time.deltaTime;
-
-        //Attack only when player in sight
-        if (PlayerInSight())
-        {
-            if (cooldownTimer >= attackCooldown)
-            {
-                //Atack
-            }
-        }
-    }
-        
-
-    private bool PlayerInSight()
-    {
-        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x,
-        new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z), 0, Vector2.left, 0, playerLayer);
-        return hit.collider != null;
-    }
+    private bool alive;
     
-    private void OnDrawGizmos()
+
+    // Follow Player
+    void FollowPlayer()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(BoxCollider.bounds.center + transform.right * range * transform.localScale.x, 
-            new Vector3(boxCollider.bounds.size.x *range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+        alive = true;
+        transform.position = player.transform.position;
+        if (alive = true)
+        {
+            Transform.Translate(Vector3, transform.position * speed * Time.deltaTime);
+        }
     }
 }
